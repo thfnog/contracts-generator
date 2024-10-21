d8fdb396e0d8ad6c40103eb1386c5809da4e5080
Nogueira <thfnog@gmail.com>
2024-10-21 14:57:24 -0300

    test to download files

--- api/contracts_page.php
+++ api/contracts_page.php
@@ -149,8 +149,8 @@ function generateContractPDF($client, $contractTypes) {
             $downloadUrl = '/api/download.php?file=' . urlencode(basename($tempDocxPath));
             
             // Redirect the user to the download URL
-            header('Location: ' . $downloadUrl);
-            //echo '<a href="' . $downloadUrl . '" download>Click here to download</a>';
+            //header('Location: ' . $downloadUrl);
+            echo '<a href="' . $downloadUrl . '" download>Click here to download</a>';
             exit();
         } else {
             echo "Error: Unable to generate the download file.";
